2b770a0b425c1c98935fb844ba2ba2e65ea0a889
Akem-Ben <akemini.ndaobong@gmail.com>
2024-04-26 15:35:41 +0100

    bug fixes

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,6 @@ import axios from './axiosSettings';
 
 export const getFastMovies = async(title, page)=>{
   try {
-      // const response = await axios.get(`/api/movies/Search/${title}`);
     const response = await axios.get(`/movies/searched?title=${title}&page=${page}`);
     return response;
   } catch (err) {
@@ -11,21 +10,22 @@ export const getFastMovies = async(title, page)=>{
 }
 
 // export const changePage = async(title, page)=>{
-//   try {
-//       const response = await axios.get(`/api/movies/Search/${title}?pageNumber=${page}`);
-//     // const response = await axios.get(`movies/searched?title=${title}&page=${page}`);
-//     return response;
-//   } catch (err) {
-//     return err.response;
-//   }
-// }
-
-export const getSingleMovieDetails = async(code)=>{
-    try {
-      const response = await axios.get(`/movies/single_movie?code=${code}`);
-      // const response = await axios.get(`/api/movies/${code}`)
-      return response;
-    } catch (err) {
-      return err.response;
-    }
-}
+  //   try {
+    //       const response = await axios.get(`/api/movies/Search/${title}?pageNumber=${page}`);
+    //     // const response = await axios.get(`movies/searched?title=${title}&page=${page}`);
+    //     return response;
+    //   } catch (err) {
+      //     return err.response;
+      //   }
+      // }
+      
+      export const getSingleMovieDetails = async(code)=>{
+        try {
+          const response = await axios.get(`/movies/single_movie?code=${code}`);
+          return response;
+        } catch (err) {
+          return err.response;
+        }
+      }
+      // const response = await axios.get(`/api/movies/Search/${title}`);
+  // const response = await axios.get(`/api/movies/${code}`)
